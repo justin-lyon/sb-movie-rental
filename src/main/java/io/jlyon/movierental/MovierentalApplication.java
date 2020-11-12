6d800bea8b083370f9f94e31b39b26474f7cedff
Justin Lyon <jml6487@gmail.com>
2020-11-11 20:25:11 -0600

    init commit

---
+++ src/main/java/io/jlyon/movierental/MovierentalApplication.java
@@ -0,0 +1,13 @@
+package io.jlyon.movierental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jlyon/movierental/MovierentalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jlyon.movierental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
