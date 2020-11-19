67c3f896d683bbf032a08dec6f7f0112b315b5d4
Justin Lyon <jml6487@gmail.com>
2020-11-18 21:22:14 -0600

    add mysql todo

--- src/main/java/io/jlyon/movierental/repository/UserRepository.java
+++ src/main/java/io/jlyon/movierental/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
+// TODO - Persist Docker MySQL Container Data outside the container.
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 	@Query("FROM UserEntity u WHERE u.email = :email")
 	UserEntity findOneByEmail(@Param("email") String email);
