2b7001a3efed186e9a75cdcbb686657113df9b1c
Justin Lyon <jml6487@gmail.com>
2021-01-10 20:05:54 -0600

    add instructions to docker script

--- local-docker-init.sh
+++ local-docker-init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Initial Setup
+# 1. Create  Volume by name `movie-rental`
 docker volume create movie-rental
+
+# 2. Create a container and image, and run the thing in one command.
 docker run --name mysql57 \
 -p 3306:3306 \
 -e MYSQL_ROOT_PASSWORD=1234 \
@@ -8,3 +12,7 @@ docker run --name mysql57 \
 -e MYSQL_PASSWORD=1234 \
 -v movie-rental:/var/lib/mysql \
 -d mysql/mysql-server:5.7
+
+# Done!
+# Daily startup:
+docker start mysql57
