55aab84d48d51f23a65a43003b2fbd61465f18c3
Justin Lyon <jml6487@gmail.com>
2021-04-30 21:39:42 -0500

    test wip - has service error with params

--- src/main/java/io/jlyon/movierental/tmdb/service/MovieService.java
+++ src/main/java/io/jlyon/movierental/tmdb/service/MovieService.java
@@ -5,7 +5,11 @@ import io.jlyon.movierental.tmdb.model.MovieDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Collections;
 
 import static io.jlyon.movierental.tmdb.TmdbWebClientConfig.WEB_CLIENT_NAME;
 
@@ -17,10 +21,14 @@ public class MovieService {
 	private WebClient.Builder wcb;
 
 	public MovieDetail getMovieById(@NotNull int movieId) {
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.put("append_to_response", Collections.singletonList("release_dates"));
+
 		return wcb.build()
 			.get()
 			.uri(uriBuilder -> uriBuilder
 				.path(PATH + "/" + movieId)
+				.queryParams(params)
 				.build())
 			.retrieve()
 			.bodyToMono(MovieDetail.class)
