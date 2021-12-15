ccf1a34e41b2849bc80f7f2dd3b0de0d46b70f25
Justin Lyon <jml6487@gmail.com>
2021-12-14 23:03:03 -0600

    uses constant jwt secret for signing

--- src/main/java/io/jlyon/movierental/security/SecurityConfig.java
+++ src/main/java/io/jlyon/movierental/security/SecurityConfig.java
@@ -9,12 +9,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
 @Component
 @Configuration
 @ConfigurationProperties("jwt")
 public class SecurityConfig {
 	public static final String BCRYPT_ENCODER = "bcrypt_encoder";
+	public static final String HMAC_SHA512 = "HmacSHA512";
 
 	@Bean(name = BCRYPT_ENCODER)
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -22,7 +25,7 @@ public class SecurityConfig {
 	}
 
 	private int durationHours;
-	private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512); // HS256, HS384, or HS512;
+	private String secret;
 
 	public int getDurationHours() {
 		return durationHours;
@@ -32,7 +35,12 @@ public class SecurityConfig {
 		this.durationHours = durationHours;
 	}
 
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
 	public SecretKey getSecretKey() {
-		return this.key;
+		byte[] decodedKey = Base64.getDecoder().decode(this.secret);
+		return new SecretKeySpec(decodedKey, HMAC_SHA512);
 	}
 }
