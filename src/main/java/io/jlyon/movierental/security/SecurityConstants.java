bb44f904d6feaab5c4042564824fb2175b9e2bee
Justin Lyon <jml6487@gmail.com>
2021-12-15 21:15:08 -0600

    clean imports

--- src/main/java/io/jlyon/movierental/security/SecurityConstants.java
+++ src/main/java/io/jlyon/movierental/security/SecurityConstants.java
@@ -1,7 +1,5 @@
 package io.jlyon.movierental.security;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 public class SecurityConstants {
 	public static final String BEARER_PREFIX = "Bearer ";
 	public static final String SIGN_UP_URL = "/signup";
