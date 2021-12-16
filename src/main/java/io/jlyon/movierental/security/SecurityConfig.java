91da4ae2eefea559a15b309b711d445806044161
Justin Lyon <jml6487@gmail.com>
2021-12-15 20:23:03 -0600

    clean up

--- src/main/java/io/jlyon/movierental/security/SecurityConfig.java
+++ src/main/java/io/jlyon/movierental/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package io.jlyon.movierental.security;
 
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
